decadcd54d234ee20b403c8fc6ce151d41223c6c
Arthur <kom9toshka@gmail.com>
2020-02-27 23:26:14 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
+  if (n < 0) {
+    n = [n].map((i)=>{return (i != 0)? i*-1: i})
+  } 
+    let str = n.toString().split('').reverse().join('')
+  
+  return Number(str)
   
 }
