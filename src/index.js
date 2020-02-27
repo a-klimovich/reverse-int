3320f372ef4edb6620ecf1a2859c2d270dc6b0eb
Arthur <kom9toshka@gmail.com>
2020-02-27 23:29:38 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ module.exports = function reverse (n) {
   if (n < 0) {
     n = [n].map((i)=>{return (i != 0)? i*-1: i})
   } 
-    let str = n.toString().split('').reverse().join('')
-  
-  return Number(str)
+ 
+  return Number(n.toString().split('').reverse().join(''))
   
 }
